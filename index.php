95c81288dafdba68b7161f6299672abada3caf3b
Mare Lopes <96207587+marehead@users.noreply.github.com>
2022-08-27 03:23:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -47,8 +47,3 @@ if ($link->query($query) === TRUE) {
 </body>
 </html>
 
-## PRATICAS
-
-```
-docker run - -name web-server -dt -p 80:80 - -mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
-```
